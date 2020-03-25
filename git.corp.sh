2818d82de07531304b8b5774fdc8afa47d4a0cee
matthewhadley <matthew.hadley@gmail.com>
2020-03-25 13:25:46 -0700

    more explicit setting of local git user

--- git.corp.sh
+++ git.corp.sh
@@ -3,31 +3,34 @@
 if [ "$1" == "commit" ];then
   GIT_CORP_ORG=$(git config --global corp.org)
   if [ ! -z "$GIT_CORP_ORG" ]; then
-    GIT_CORP_USER=$(git config --global corp.user)
     GIT_CORP_EMAIL=$(git config --global corp.email)
     CONFIG=$(git rev-parse --show-toplevel 2> /dev/null)
-
     IFS=';' read -ra ADDR <<< "$GIT_CORP_ORG"
     for i in "${ADDR[@]}"; do
       CORP_GIT=$(grep "${i}" ${CONFIG}/.git/config | wc -l)
       if [ "$CORP_GIT" -ge "1" ]; then
-        NOTICE="notice: corp repo detected\n"
-        if [ $(git config user.email) != "$GIT_CORP_EMAIL" ]; then
-          echo -e "${NOTICE}- updating local git user to be ${GIT_CORP_USER}"
-          NOTICE=""
+        GIT_CORP_USER=$(git config --global corp.user)
+        if [[ $(git config user.name) != "$GIT_CORP_USER" || $(git config user.email) != "$GIT_CORP_EMAIL" ]]; then
+          echo -e "notice: corp repo detected"
+          echo -e "updating local git user to be ${GIT_CORP_USER}"
           git config --local user.name "$GIT_CORP_USER"
           git config --local user.email "$GIT_CORP_EMAIL"
         fi
 
         if [ ! -f "${CONFIG}/.git/hooks/commit-msg" ]; then
-          echo -e "${NOTICE}- adding commit-msg hook"
+          echo -e "- adding commit-msg hook"
           DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
           cp "$DIR/commit-msg" "${CONFIG}/.git/hooks/"
-          NOTICE=""
-          echo ""
         fi
 
         break
+      else
+        GIT_EMAIL=$(git config --global personal.email)
+        GIT_USER=$(git config --global personal.user)
+        echo -e "notice: non-corp repo detected"
+        echo -e "- updating local git user to be ${GIT_USER}"
+        git config --local user.name "$GIT_USER"
+        git config --local user.email "$GIT_EMAIL"
       fi
     done
   fi
